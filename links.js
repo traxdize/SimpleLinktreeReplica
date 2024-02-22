989436a2cae4c151825d68b18efa68c51adb39e9
traxdize <kenneth.a.jims@gmail.com>
2024-02-22 10:31:20 +0700

    Yes

---
+++ links.js
@@ -0,0 +1,18 @@
+export const links = [
+    {
+        name: "Profil UBV-ITB",
+        link: "https://drive.google.com/file/d/16zTcFa2kcLAKmAJyK4JsHiG418HRAHmc/view?usp=drivesdk",
+    },
+    {
+        name: "Teaser",
+        link: "https://drive.google.com/file/d/1v0bzpyoUujw0HIYMIdTcliz5mN6ToPJL/view",
+    },
+    {
+        name: "Daftar",
+        link: "https://docs.google.com/forms/d/e/1FAIpQLSdwuYHCZqeeS-zCAKbV4oxLuc2KCZp43Bm2dna0Ei_x0GPH7g/viewform",
+    },
+    {
+        name: "Instagram",
+        link: "https://www.instagram.com/unitbolavoliitb?igsh=dHRlYWFtNWgzam9u",
+    }
+]
